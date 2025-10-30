200149c78b284c0c7cbc48d8d2cdb78f377c3bc4
InformedGuy <184560653+InformedGuy@users.noreply.github.com>
2025-10-30 08:09:03 +0300

    Completed fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+
+    // Return the nth member of the fibonacci series
+
+    if (member < 0) {
+        return "OOPS";
+    }
+
+    let array = [0, 1];
+
+    if (member > 1) {
+        for (let i = 2; i <= member; i++) {
+            let newNumber = array[i - 1] + array[i - 2];
+            array.push(newNumber);
+        }
+    }
+
+    return array[member];
 
 };
 
