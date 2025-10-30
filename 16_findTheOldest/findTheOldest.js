d09cfb50ff92862fa9f8df501988cc3508f32673
InformedGuy <184560653+InformedGuy@users.noreply.github.com>
2025-10-30 12:05:50 +0300

    Completed findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let peopleAge = people.map(person => {
+        let age = 0;
+        if ("yearOfDeath" in person) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            const currentYear = new Date().getFullYear();
+            age = currentYear - person.yearOfBirth;
+        }
+
+        return { ...person, age: age };
+    });
+
+    let oldest = peopleAge.reduce((prev, current) => {
+        return (prev.age > current.age) ? prev : current;
+    });
+
+    delete oldest.age;
+
+    return oldest;
 
 };
 
